e0bf97e83c445eaee49e460c71db64b4cf973b9e
김정민 <dsaqaz421@naver.com>
2020-07-01 12:14:02 +0900

    map의 다형성

--- map, filter, reduce/map.js
+++ map, filter, reduce/map.js
@@ -39,3 +39,28 @@ const map = (f, iter) =>{
 
 // map을 사용해서 값을 가져오는 예시
 log(map(p => p.name, products));
+
+
+/* 이터러블 프로토콜을 따른 map의 다형성 */
+log([1, 2, 3].map(a => a+1));
+
+// document.queryselectorall을 사용해 map을 사용하면 오류가 남
+// queryselectorall은 array iterator를 사용하기 때문임
+function *gen(){
+    yield 2;
+    yield 3;
+    yield 4;
+}
+log(map(a => a* a, gen()));
+
+
+/* 다형성 2 */
+let m = new Map();
+m.set('a', 10);
+m.set('b', 20);
+const it = m[Symbol.iterator]();
+line1;
+log(it.next());
+log(it.next());
+log(it.next());
+log(new Map(map(([k, a]) => [k, a * 2], m)));    // 이렇게도 응용해서 새로운 맵 객체를 만들 수 있음

d11e155ecb1449b6752fb0d498916f87fa96b1d0
김정민 <dsaqaz421@naver.com>
2020-07-02 12:22:44 +0900

    reduce를 사용한 예제

--- map, filter, reduce/reduce.js
+++ map, filter, reduce/reduce.js
@@ -1,6 +1,12 @@
 const log = console.log;
 
-
+const products = [
+    {name : '반팔티', price : 15000},
+    {name : '긴팔티', price : 20000},
+    {name : '핸드폰케이스', price : 15000},
+    {name : '후드티', price : 30000},
+    {name : '바지', price : 25000}
+];
 
 /* reduce */
 const nums = [1, 2, 3, 4, 5];
@@ -30,4 +36,7 @@ log(reduce(add, 0, [1,2,3,4,5]));               // return 15
 log(add(add(add(add(add(0,1),2),3),4),5));      // 위의 reduce를 풀어씀
 log(reduce(add, [1, 2, 3, 4, 5]));
 
-// reduce 함수를 통해 누적시키는 예제
+
+// products의 값을 모두 더하는 reduce 함수
+log("=====================================");
+log(reduce((total_price, products) => total_price + products.price, 0 , products));
